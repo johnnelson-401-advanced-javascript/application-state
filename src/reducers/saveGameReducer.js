60ac1ff9c7e5862d01d563999dd55a5aa2e05d9e
JOHN NELSON <JohnNelson4850@gmail.com>
2019-11-12 19:09:09 -0800

    savedGameReducer

--- src/reducers/saveGameReducer.js
+++ src/reducers/saveGameReducer.js
@@ -1,15 +1,18 @@
-const initialState = {
-  coffees: 0,
-  snacks: 0,
-  naps: 0,
-  studies: 0,
-  walks: 0
-};
+const initialState = [
+  {
+    savedGame: {
+      coffees: 0,
+      snacks: 0,
+      naps: 0,
+      studies: 0,
+      walks: 0
+    }
+  }];
 
 export default function saveGameReducer(state = initialState, action) {
   switch(action.type) {
     case 'SAVE_GAME':
-      return { ...state };
+      return [...state, action.payload];
     case 'LOAD_GAME':
       return state = { ...state };
     default:
---
+++ src/selectors/savedGames.js
@@ -0,0 +1,1 @@
+export const savedGames = state => state.savedGames;
